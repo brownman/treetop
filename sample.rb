82bcadffca7e095dccf5d3ee32655792b5b88bc3
Daniel Schmidt <dsci@code79.net>
2010-11-03 17:03:14 +0100

    improve grammar played around with SyntaxNode monkey patching

--- sample.rb
+++ sample.rb
@@ -7,6 +7,23 @@ require "number"
 require "simple_test"
 require "product_test"
 
+module IntegerExtension
+ 
+  
+  def pow(k)
+    puts "#{self.text_value.to_i}/#{k.text_value}"
+    l = elements.map{|n| n.text_value.gsub("^", "**")}
+		return eval(l.to_s)
+  end
+  
+  def debug
+    puts self.text_value
+  end
+  
+end
+
+
+Treetop::Runtime::SyntaxNode.send(:include, IntegerExtension)
 
 puts "grammar with [1-6]"
 
@@ -17,7 +34,7 @@ pp e
 p e.value
 
 
-f = ProductTestParser.new.parse("4*3^9")
+f = ProductTestParser.new.parse("4*3^2")
 
 pp f
 
